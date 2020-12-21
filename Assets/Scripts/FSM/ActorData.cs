515223e5bdf15aa8339d829eeef59ed4c8890bfb
nikitakaralyus <karalyus130204@gmail.com>
2020-12-21 15:33:16 +0300

    Update ActorData class

--- Assets/Scripts/FSM/ActorData.cs
+++ Assets/Scripts/FSM/ActorData.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Actor Data", menuName = "Actor/Data")]
 public class ActorData : ScriptableObject
 {
+    [Header("Movement")]
     public float LaneChangeSpeed;
 
+    [Header("Jump")]
     public float JumpForce;
 
+    [Header("Slide")]
+    public float StandColliderHeight;
+    public float SlideColliderHeight;
+
+    [Header("Checks")]
     public float GroundCheckRadius;
     public LayerMask WhatIsGround;
+
 }
