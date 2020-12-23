99ab95c9ff71a218a4e0b03ddcdc146452da2fe1
nikitakaralyus <karalyus130204@gmail.com>
2020-12-23 21:10:33 +0300

    Basic Chunk Replace

---
+++ Assets/Scripts/Chunks/ChunkPlacer.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections.Generic;
+
+public class ChunkPlacer : MonoBehaviour
+{
+    public Transform Player { get; set; }
+    public Chunk StartingChunk => _startingChunk.GetComponent<Chunk>();
+
+    [SerializeField] private float _prespawnDistance;
+    [SerializeField] private int _maxChunksOnScene = 3;
+    [SerializeField] private GameObject _startingChunk;
+    [SerializeField] private ChunkPool _pool;
+
+    public List<Chunk> SpawnedChunks { get; private set; }
+
+    private void Awake()
+    {
+        SpawnedChunks = new List<Chunk>();
+        SpawnedChunks.Add(StartingChunk);
+    }
+
+    private void Update()
+    {
+        int index = SpawnedChunks.Count - 1;
+
+        if (Player.position.z > SpawnedChunks[index].transform.position.z - _prespawnDistance)
+        {
+            var chunk = Spawn(SpawnedChunks[index].EndPoint.position);
+            SpawnedChunks.Add(chunk);
+        }
+
+        DisposeChunks();
+
+    }
+
+    private Chunk Spawn(Vector3 previuosChunkEndPosition)
+    {
+        var chunk = _pool.GetChunk();
+        chunk.SetSpawnPosition(previuosChunkEndPosition);
+        chunk.gameObject.SetActive(true);
+
+        SpawnedChunks.Add(chunk);
+
+        return chunk;
+    }
+
+    //todo: dispose chunks
+    private void DisposeChunks()
+    {
+
+    }
+}
+
--- Assets/Scripts/Processors/TrackProcessor.cs
+++ Assets/Scripts/Processors/TrackProcessor.cs
@@ -1,61 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
-using System.Linq;
 
 #pragma warning disable 0649
-public class TrackProcessor : MonoSingletone<TrackProcessor>
-{
-    private const float offset = 15.0f;
-
+public class TrackProcessor : MonoBehaviour
+{ 
     public float Speed { get; private set; }
 
     [SerializeField] private float _startingSpeed;
 
-    [SerializeField] private GameObject _startingChunk;
-    [SerializeField] private ChunkSpawner _chunkSpawner;
+    [SerializeField] private ChunkPlacer _chunkPlacer;
     [SerializeField] private Actor _player;
-
-    private List<Chunk> _spawnedChunks = new List<Chunk>();
 
     private void Start()
     {
         Speed = _startingSpeed;
+        _chunkPlacer.Player = _player.transform;
 
-        var chunk = _startingChunk.GetComponent<Chunk>();
-        _spawnedChunks.Add(chunk);
-
+        var chunk = _chunkPlacer.StartingChunk;
         _player.LaneOffset = chunk.Ground.GetLaneOffset();
     }
 
     private void Update()
     {
         MoveChunks();
-        ReplaceChunks();
-    }
-
-    public void AddChunk()
-    {
-        var lastChunkEnd = _spawnedChunks.Last().EndPoint;
-        var chunk = _chunkSpawner.Spawn(lastChunkEnd.position);
-        _spawnedChunks.Add(chunk);
     }
 
     private void MoveChunks()
     {
-        foreach (var chunk in _spawnedChunks)
+        foreach (var chunk in _chunkPlacer.SpawnedChunks)
         {
             chunk.Move(Vector3.back, Speed);
         }
     }
 
-    private void ReplaceChunks()
-    {
-        if (_player.transform.position.z > _spawnedChunks.Last().EndPoint.position.z - offset)
-        {
-            AddChunk();
-        }
-    }
 
 }
