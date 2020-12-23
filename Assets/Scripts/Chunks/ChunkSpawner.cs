f8d7a4daf6ec57a545fe1179660f538f4800b204
nikitakaralyus <karalyus130204@gmail.com>
2020-12-23 19:09:29 +0300

    Update ChunkSpawner.cs

--- Assets/Scripts/Chunks/ChunkSpawner.cs
+++ Assets/Scripts/Chunks/ChunkSpawner.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class ChunkSpawner : MonoBehaviour
 {
-    [SerializeField] private TrackProcessor _trackProcessor;
     [SerializeField] private ChunkPool _chunkPool;
 
-    public void Spawn()
+    public Chunk Spawn(Vector3 position)
     {
         var chunk = _chunkPool.GetChunk();
-        chunk.transform.parent = this.transform;
+        chunk.SetSpawnPosition(position);
         chunk.gameObject.SetActive(true);
-        _trackProcessor.AvailableChunks.Add(chunk);
+
+        return chunk;
     }
 }
