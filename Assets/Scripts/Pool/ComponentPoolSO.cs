4be1659928c946b35898b5db6090ebb54a7fc0e1
nikitakaralyus <karalyus130204@gmail.com>
2021-01-19 15:14:50 +0300

    Added Pool System

---
+++ Assets/Scripts/Pool/ComponentPoolSO.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+namespace MarkOne.Pool
+{
+    public abstract class ComponentPoolSO<T> : PoolSO<T> where T : Component
+    {
+        private Transform _poolRoot;
+        private Transform _parent;
+
+        public Transform PoolRoot
+        {
+            get
+            {
+                if (_poolRoot == null)
+                {
+                    _poolRoot = new GameObject(name).transform;
+                    _poolRoot.SetParent(_parent);
+                }
+
+                return _poolRoot;
+            }
+        }
+
+        public void SetParent(Transform parent)
+        {
+            _parent = parent;
+            PoolRoot.SetParent(parent);
+        }
+
+        public override T Request()
+        {
+            T member = base.Request();
+            member.gameObject.SetActive(true);
+            return member;
+        }
+
+        public override void Return(T member)
+        {
+            member.transform.SetParent(PoolRoot.transform);
+            member.gameObject.SetActive(false);
+            base.Return(member);
+        }
+
+        protected override T Create()
+        {
+            T newMember = base.Create();
+            newMember.transform.SetParent(PoolRoot.transform);
+            newMember.gameObject.SetActive(false);
+            return newMember;
+        }
+
+        public override void OnDisable()
+        {
+            base.OnDisable();
+
+            if (_poolRoot != null)
+            {
+#if UNITY_EDITOR
+                DestroyImmediate(_poolRoot.gameObject);
+#else
+				Destroy(_poolRoot.gameObject);
+#endif
+            }
+        }
+    }
+}
