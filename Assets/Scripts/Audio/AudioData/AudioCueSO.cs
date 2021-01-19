74393486e5b7072f575efd981cfb3bcc13cc7e0a
nikitakaralyus <karalyus130204@gmail.com>
2021-01-19 18:49:41 +0300

    Completed Audio System

---
+++ Assets/Scripts/Audio/AudioData/AudioCueSO.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "newAudioCue", menuName = "Audio/Audio Cue")]
+public class AudioCueSO : ScriptableObject
+{
+    public bool IsLooping = false;
+    [SerializeField] private AudioClipsGroup[] _audioClipGroups;
+
+    public AudioClip[] GetClips()
+    {
+        var numberOfClips = _audioClipGroups.Length;
+        AudioClip[] resultingClips = new AudioClip[numberOfClips];
+
+        for (int i = 0; i < numberOfClips; i++)
+        {
+            resultingClips[i] = _audioClipGroups[i].GetNextClip();
+        }
+
+        return resultingClips;
+    }
+}
---
+++ Assets/Scripts/Channels/AudioCueEventChannelSO.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Events/AudioCue Event Channel")]
+public class AudioCueEventChannelSO : ScriptableObject
+{
+    public UnityAction<AudioCueSO, AudioConfigurationSO, Vector3> OnAudioCueRequested;
+
+    public void RaiseEvent(AudioCueSO audioCue, AudioConfigurationSO audioConfiguration, Vector3 positionInSpace = default)
+    {
+		if (OnAudioCueRequested != null)
+		{
+			OnAudioCueRequested.Invoke(audioCue, audioConfiguration, positionInSpace);
+		}
+		else
+		{
+			Debug.LogWarning("An AudioCue was requested, but nobody picked it up. " +
+				"Check why there is no AudioManager already loaded, " +
+				"and make sure it's listening on this AudioCue Event channel.");
+		}
+	}
+}
