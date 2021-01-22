e800c3c3a6be1a87935b5dd527e55d9bf4501c16
nikitakaralyus <karalyus130204@gmail.com>
2021-01-22 13:53:58 +0300

    Started Input System Rebuild

---
+++ Assets/Scripts/Input/InputReader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(fileName = "InputReader", menuName = "Game/Input Reader")]
+public class InputReader : ScriptableObject
+{
+    public event UnityAction<MoveDirection> moveEvent = delegate { };
+    public event UnityAction jumpEvent = delegate { };
+    public event UnityAction slideEvent = delegate { };
+
+    public enum MoveDirection
+    {
+        Left = -1,
+        Right = 1
+    }
+}
