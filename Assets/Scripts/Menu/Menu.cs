a7b6f49cbe4333643175beb7cdc6320d2a9df3a5
nikitakaralyus <karalyus130204@gmail.com>
2021-01-26 13:19:12 +0300

    Small Visual Emprovments

--- Assets/Scripts/Menu/Menu.cs
+++ Assets/Scripts/Menu/Menu.cs
@@ -9,9 +9,10 @@ public class Menu : MonoBehaviour
 
     [Header("Locations To Load")]
     [SerializeField] private GameSceneSO[] _locationsToLoad;
+    [SerializeField] private bool _enableLoadingScreen = false;
 
     public void OnPlayButtonClicked()
     {
-        _loadEventChannel.RaiseEvent(_locationsToLoad, false);
+        _loadEventChannel.RaiseEvent(_locationsToLoad, _enableLoadingScreen);
     }
 }
