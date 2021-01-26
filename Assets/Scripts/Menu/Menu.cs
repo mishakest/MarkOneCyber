1782ffe759a56445cad78dc0743d27dc18403b1a
nikitakaralyus <karalyus130204@gmail.com>
2021-01-26 13:12:29 +0300

    Deleted Unused buttons classes

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    [Header("Channel")]
+    [SerializeField] private LoadEventChannelSO _loadEventChannel;
+
+    [Header("Locations To Load")]
+    [SerializeField] private GameSceneSO[] _locationsToLoad;
+
+    public void OnPlayButtonClicked()
+    {
+        _loadEventChannel.RaiseEvent(_locationsToLoad, false);
+    }
+}
