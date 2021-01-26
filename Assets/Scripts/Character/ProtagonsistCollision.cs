1cf00142469d2964af7ac782f51fc22971c1ab49
nikitakaralyus <karalyus130204@gmail.com>
2021-01-26 14:44:48 +0300

    Added HandleComponent Method

--- Assets/Scripts/Character/ProtagonsistCollision.cs
+++ Assets/Scripts/Character/ProtagonsistCollision.cs
@@ -1,8 +1,20 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ProtagonsistCollision : MonoBehaviour
 {
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
 
+    private void HandleComponent<T>(GameObject gameObject, Action<T> handler)
+    {
+        var component = gameObject.GetComponent<T>();
+
+        if (component != null)
+            handler?.Invoke(component);
+    }
 }
