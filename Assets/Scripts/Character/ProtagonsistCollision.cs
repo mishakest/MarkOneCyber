bab6dc88f96632998d740e1e62e44f6df51e1fb7
nikitakaralyus <karalyus130204@gmail.com>
2021-01-26 14:52:19 +0300

    Moved HandleComponent method to the extensioins class

--- Assets/Scripts/Character/ProtagonsistCollision.cs
+++ Assets/Scripts/Character/ProtagonsistCollision.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Extensions;
 
 public class ProtagonsistCollision : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision)
     {
-        
-    }
 
-    private void HandleComponent<T>(GameObject gameObject, Action<T> handler)
-    {
-        var component = gameObject.GetComponent<T>();
-
-        if (component != null)
-            handler?.Invoke(component);
     }
 }
