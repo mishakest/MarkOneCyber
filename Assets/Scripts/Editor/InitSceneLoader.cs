67f5bfa6b83a5c1b42d52b8b942d855aaf597831
nikitakaralyus <karalyus130204@gmail.com>
2021-02-18 21:18:00 +0300

    Added Tram mesh and Init Scene Default Load System

---
+++ Assets/Scripts/Editor/InitSceneLoader.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+[InitializeOnLoad]
+public static class InitSceneLoader
+{
+    static InitSceneLoader()
+    {
+        EditorApplication.playModeStateChanged += LoadDefaultScene;
+    }
+
+    static void LoadDefaultScene(PlayModeStateChange state)
+    {
+        switch (state)
+        {
+            case PlayModeStateChange.ExitingEditMode:
+                EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+                break;
+            case PlayModeStateChange.EnteredPlayMode:
+                EditorSceneManager.LoadScene(0, LoadSceneMode.Additive);
+                break;
+        }
+    }
+}
+#endif
