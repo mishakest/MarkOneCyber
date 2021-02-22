22266dd5ceb0d1e7c01c2d43f358933a3dbff128
nikitakaralyus <karalyus130204@gmail.com>
2021-02-22 14:10:19 +0300

    Got rid of commented code in Chunks Placer

--- Assets/Scripts/Chunks/ChunksPlacer.cs
+++ Assets/Scripts/Chunks/ChunksPlacer.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class ChunksPlacer : MonoBehaviour
 {
     [SerializeField] private TrackProcessorProviderSO _provider = default;
-    
+
     [SerializeField] private Chunk _startingChunk = default;
     [SerializeField] private ChunksPool _chunksPool = default;
 
@@ -69,69 +69,3 @@ public class ChunksPlacer : MonoBehaviour
         return _spawnedChunks.Last().EndPoint.position - currentChunk.StartPoint.localPosition;
     }
 }
-
-//public class ChunksPlacers : MonoBehaviour
-//{
-//    [SerializeField] private TrackProcessorProviderSO _provider = default;
-
-//    [SerializeField] private Transform _player;
-
-//    [Header("Chunks")]
-//    [SerializeField] private ChunksPool _chunksPool;
-//    [SerializeField] private Chunk _startingChunk;
-
-//    [Header("Preferences")]
-//    [SerializeField] private float _prespawnDistance = 150.0f;
-//    [SerializeField] private int _maxChunksOnScene = 3;
-
-//    public List<Chunk> SpawnedChunks { get; private set; }
-
-//    private void Start()
-//    {
-//        SpawnedChunks = new List<Chunk>();
-//        SpawnedChunks.Add(_startingChunk);
-//        _provider.ChunksMoveables.Add(_startingChunk);
-//    }
-
-//    private void Update()
-//    {
-//        if (_player.position.z > GetLastChunk().EndPoint.position.z - _prespawnDistance)
-//        {
-//            SpawnChunk();
-//        }
-
-//        if (SpawnedChunks.Count > _maxChunksOnScene)
-//            DistinctChunk();
-//    }
-
-//    private void SpawnChunk()
-//    {
-//        var chunk = _chunksPool.Request();
-//        chunk.gameObject.SetActive(true);
-//        chunk.transform.position = GetLastChunk().EndPoint.position - chunk.StartPoint.localPosition;
-//        SpawnedChunks.Add(chunk);
-//        _provider.ChunksMoveables.Add(chunk);
-//    }
-
-//    private void DistinctChunk()
-//    {
-//        var chunk = SpawnedChunks[0];
-//        chunk.gameObject.SetActive(false);
-
-//        foreach (var onRunChunk in _chunksPool.CreatedOnRunChunks)
-//        {
-//            if (chunk.gameObject == onRunChunk.gameObject)
-//            {
-//                SpawnedChunks.RemoveAt(0);
-//                _provider.ChunksMoveables.RemoveAt(0);
-//                _chunksPool.DestroyCreatedChunk(chunk);
-//                return;
-//            }
-//        }
-
-//        SpawnedChunks.RemoveAt(0);
-//        _provider.ChunksMoveables.RemoveAt(0);
-//    }
-
-//    private Chunk GetLastChunk() => SpawnedChunks[SpawnedChunks.Count - 1];
-//}
